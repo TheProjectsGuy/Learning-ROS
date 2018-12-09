e30ea8554fde49cc564ba1c6587c08e33b751106
Avneesh Mishra <123avneesh@gmail.com>
2018-12-09 18:53:36 +0530

    Added code for C++ and Python publisher and subscriber

---
+++ ros_ws/build/intro_pkg1/CMakeFiles/NumberPublisher.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumberPublisher.dir/src/NumberPublisher.cpp.o"
+  "/home/stark/ROS_workspaces/ros_ws/devel/lib/intro_pkg1/NumberPublisher.pdb"
+  "/home/stark/ROS_workspaces/ros_ws/devel/lib/intro_pkg1/NumberPublisher"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NumberPublisher.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ros_ws/build/intro_pkg1/CMakeFiles/NumberSubscriber.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NumberSubscriber.dir/src/NumberSubscriber.cpp.o"
+  "/home/stark/ROS_workspaces/ros_ws/devel/lib/intro_pkg1/NumberSubscriber.pdb"
+  "/home/stark/ROS_workspaces/ros_ws/devel/lib/intro_pkg1/NumberSubscriber"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NumberSubscriber.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
