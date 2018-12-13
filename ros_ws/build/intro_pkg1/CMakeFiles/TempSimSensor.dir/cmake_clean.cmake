f9101188034221becfe7be563eace4ea083c3e79
Avneesh Mishra <123avneesh@gmail.com>
2018-12-13 22:23:42 +0530

    Finished RViZ tutorial 1

---
+++ ros_ws/build/intro_pkg1/CMakeFiles/TempSimSensor.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TempSimSensor.dir/src/TempSensor.cpp.o"
+  "/home/stark/ROS_workspaces/ros_ws/devel/lib/intro_pkg1/TempSimSensor.pdb"
+  "/home/stark/ROS_workspaces/ros_ws/devel/lib/intro_pkg1/TempSimSensor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/TempSimSensor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
