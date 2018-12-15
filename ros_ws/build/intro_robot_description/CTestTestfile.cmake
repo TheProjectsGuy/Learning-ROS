2d856cf1af797de48622bfaff85ed7543b1e5e5a
Avneesh Mishra <123avneesh@gmail.com>
2018-12-15 11:08:59 +0530

    Added first robot URDF

---
+++ ros_ws/build/intro_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/stark/ROS_workspaces/ros_ws/src/intro_robot_description
+# Build directory: /home/stark/ROS_workspaces/ros_ws/build/intro_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
